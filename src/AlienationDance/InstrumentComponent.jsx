e46765f0daf1db71ebd2361e4f1fd21a36f58caf
Francisco Rafart <francisco.rafart@gmail.com>
2021-02-21 16:04:43 -0500

    Implement container drag constraint

--- src/AlienationDance/InstrumentComponent.jsx
+++ src/AlienationDance/InstrumentComponent.jsx
@@ -2,8 +2,8 @@ import React, {useState} from 'react';
 import styled from 'styled-components';
 
 const Instrument = styled.div`
-    height: 100px;
-    width: 100px;
+    height: ${props => `${props.height}`}px;
+    width: ${props => `${props.width}`}px;;
     display: block;
     border: 1px solid black;
     position: absolute;
@@ -12,20 +12,76 @@ const Instrument = styled.div`
     cursor: pointer;
 `;
 
-const InstrumentComponent = ({name, startPosition}) => {
+const inContainer = (
+    position, 
+    height, 
+    width,
+    limits,
+) => {
+    let horizontal = 0;
+    if (position.left < limits.leftLimit){
+        horizontal = limits.leftLimit
+    } else if (position.left + width > limits.rightLimit) {
+        horizontal = limits.rightLimit - width;
+    } else {
+        horizontal = position.left;
+    }
+
+    let vertical = 0;
+
+    if (position.top < limits.topLimit) {
+        vertical = limits.topLimit;
+    } else if (position.top + height > limits.bottomLimit) {
+        vertical = limits.bottomLimit - height;
+    } else {
+        vertical = position.top;
+    }
+
+    return {
+        left: horizontal,
+        top: vertical,
+    }
+}
+const InstrumentComponent = ({
+    name, 
+    startPosition, 
+    height, 
+    width,
+    limits,
+}) => {
     const [position, setPosition] = useState({top: startPosition.top, left: startPosition.left});
 
     const onDrag = e => {
-        console.log('e', e)
-        setPosition({left: Number(e.clientX), top: Number(e.clientY)})
+        // console.log('e', e)
+        setPosition(
+            inContainer(
+                {left: Number(e.clientX), top: Number(e.clientY)}, 
+                height,
+                width,
+                limits,
+            )
+        )
     }
 
     const onDrop = e => {
-        console.log('drop e', e)
-        setPosition({left: Number(e.clientX), top: Number(e.clientY)})
+        setPosition(
+            inContainer(
+                {left: Number(e.clientX), top: Number(e.clientY)}, 
+                height,
+                width,
+                limits,
+            )
+        )
     }
     return(
-        <Instrument onDrag={onDrag} onDragEnd={onDrop} position={position}>{name}</Instrument>
+        <Instrument 
+            onClick={() => console.log('click')} 
+            onDrag={onDrag} 
+            onDragEnd={onDrop}
+            position={position}
+            height={height}
+            width={width}
+        >{name}</Instrument>
     );
 };
 
--- src/AlienationDance/index.jsx
+++ src/AlienationDance/index.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import InstrumentComponent from './InstrumentComponent';
+import styled from 'styled-components';
+
+const MixerContainer = styled.div`
+    margin: 100px 0 0 100px;
+    width: 800px;
+    height: 600px;
+    border: 1px solid gray;
+`;
 
 const AlienationDance = () => {
 
+    // TODO: Determine limits from size and position of Mixer Container
+    const limits = {
+        rightLimit: 900,
+        leftLimit: 100,
+        topLimit: 100,
+        bottomLimit: 700,
+    }
+
     return(
         <div>
-            <InstrumentComponent name='The Stick' startPosition={{left: 100, top: 100}}/>
-            <InstrumentComponent name='Drums' startPosition={{left: 200, top: 100}}/>
-            <InstrumentComponent name='Synths' startPosition={{left: 300, top: 100}}/>
-            <InstrumentComponent name='Guitar' startPosition={{left: 400, top: 100}}/>
+            <MixerContainer>
+                <InstrumentComponent name='The Stick' startPosition={{left: 100, top: 100}} height={50} width={50} limits={limits}/>
+                <InstrumentComponent name='Drums' startPosition={{left: 200, top: 100}} height={50} width={50} limits={limits}/>
+                <InstrumentComponent name='Synths' startPosition={{left: 300, top: 100}} height={50} width={50} limits={limits}/>
+                <InstrumentComponent name='Guitar' startPosition={{left: 400, top: 100}} height={50} width={50} limits={limits}/>
+            </MixerContainer>
         </div>
     )
 };
