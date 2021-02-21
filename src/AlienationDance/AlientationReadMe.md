c23a87b5f4836c2f40a0920c6e9353477b3ef084
Francisco Rafart <francisco.rafart@gmail.com>
2021-02-21 16:29:01 -0500

    Add Mixer Container constraints and window size responsiveness

---
+++ src/AlienationDance/AlientationReadMe.md
@@ -0,0 +1,20 @@
+TODO
+
+Interface
+
+Determine padding / limits dynamically to window size for a better experience on Desktop
+Dinamically modify instrument position if size of screen changes
+
+B. Audio
+    1. Load files and play audio (web audio api)
+    2. Simple transport for starting, pause, stop
+    3. Time tracker (for trigerring different behaviors in song)
+    4. Master FX knobs for reverb / delay
+    5. Keyboard synth for solo part
+
+C. Visuals
+    1. Backdrop animation or video?
+    2. Icons and animation
+    3. Keyboard synth feedback
+
+D. Add Stripe for donations
--- src/AlienationDance/index.jsx
+++ src/AlienationDance/index.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import InstrumentComponent from './InstrumentComponent';
 import styled from 'styled-components';
 
 const MixerContainer = styled.div`
-    margin: 100px 0 0 100px;
-    width: 800px;
-    height: 600px;
+    margin: 30px 30px 30px 30px;
+    width: ${props => `${props.width - 60}px`};
+    height: ${props => `${props.height - 60}px`};
     border: 1px solid gray;
 `;
 
 const AlienationDance = () => {
+    const windowSize = useWindowSize();
+    const height = windowSize.height;
+    const width = windowSize.width;
 
-    // TODO: Determine limits from size and position of Mixer Container
     const limits = {
-        rightLimit: 900,
-        leftLimit: 100,
-        topLimit: 100,
-        bottomLimit: 700,
+        rightLimit: width - 30,
+        leftLimit: 30,
+        topLimit: 30,
+        bottomLimit: height - 30,
     }
 
     return(
         <div>
-            <MixerContainer>
+            <MixerContainer height={height} width={width}>
                 <InstrumentComponent name='The Stick' startPosition={{left: 100, top: 100}} height={50} width={50} limits={limits}/>
                 <InstrumentComponent name='Drums' startPosition={{left: 200, top: 100}} height={50} width={50} limits={limits}/>
                 <InstrumentComponent name='Synths' startPosition={{left: 300, top: 100}} height={50} width={50} limits={limits}/>
@@ -30,5 +32,37 @@ const AlienationDance = () => {
         </div>
     )
 };
+
+// Hook
+function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+  
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+      
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+  
+    return windowSize;
+  }
 
 export default AlienationDance;
