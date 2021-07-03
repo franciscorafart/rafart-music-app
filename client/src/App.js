aae09a13251c046deb229737aa6ac42ff16abe1a
Francisco Rafart <francisco.rafart@gmail.com>
2021-07-03 14:47:12 -0400

    Exact path for landing

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path='/'>
+          <Route exact path='/'>
             <AppLanding />
             {/* <AlienationDance /> */}
           </Route>
