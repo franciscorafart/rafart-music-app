537d77450ca0cbae1264b05ad50af05ebc3836c3
Francisco Rafart <francisco.rafart@gmail.com>
2024-04-03 11:37:20 -0400

    Separate app from index for easier testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,38 +1,36 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import express from "express";
-import cors from "cors";
-import authentication from "./routes/auth";
-import stripeRoute from "./routes/stripe";
-import alienationRoute from "./routes/alienation";
-
-const isProduction = process.env.NODE_ENV === "production";
-
-const app = express();
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-// Secure Cors
-const origin = {
-  origin: isProduction ? "https://www.<front-end-origin>.com/" : "*",
-};
-app.use(cors(origin));
+import * as http from "http";
+import { AppDataSource } from "./data-source";
+import redisClient from "./config/redis-client";
+import app from "./app";
+import { logError } from "./helpers/logger";
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Runing on port ${port}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  // NOTE: No DB init on test environment.
+  // Supertest initializes it for HTTP endpoint integration tests
+  // and the code initializes it for entities functions
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  AppDataSource.initialize()
+    .then(async () => {
+      await redisClient.connect();
 
-// routes
-app.use("/auth", authentication);
-app.use("/stripe", stripeRoute);
-app.use("/alienation", alienationRoute);
+      redisClient.on("error", (error) => console.error(`Ups : ${error}`));
+      redisClient.on("connect", async () => {
+        console.log("Connected to our redis instance!");
+      });
+
+      const httpServer = http.createServer(app);
+
+      httpServer.listen(port, () => {
+        console.log(`Http running on port ${port}. DB connected`);
+      });
+    })
+    .catch((error) => {
+      logError(`Connection error: ${error}`);
+    });
+}
+
+export default app;
